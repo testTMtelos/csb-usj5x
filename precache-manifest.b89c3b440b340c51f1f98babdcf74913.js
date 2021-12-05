d2d8256b23037a0767396066698e36c21f70f234
Code Sandbox <hello@codesandbox.io>
2021-12-05 22:38:13 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.b89c3b440b340c51f1f98babdcf74913.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e819aa65c1864684979048328c8f1ecb",
+    "url": "https://testtmtelos.github.io/csb-usj5x/index.html"
+  },
+  {
+    "revision": "254fc8a490cb1ab5c4b7",
+    "url": "https://testtmtelos.github.io/csb-usj5x/static/css/2.dfd58508.chunk.css"
+  },
+  {
+    "revision": "42a4a2492777274d2ec1",
+    "url": "https://testtmtelos.github.io/csb-usj5x/static/css/main.a11625c5.chunk.css"
+  },
+  {
+    "revision": "254fc8a490cb1ab5c4b7",
+    "url": "https://testtmtelos.github.io/csb-usj5x/static/js/2.6eb054e0.chunk.js"
+  },
+  {
+    "revision": "42a4a2492777274d2ec1",
+    "url": "https://testtmtelos.github.io/csb-usj5x/static/js/main.c4914a46.chunk.js"
+  },
+  {
+    "revision": "b8b68d93a55a3304794d",
+    "url": "https://testtmtelos.github.io/csb-usj5x/static/js/runtime~main.f8f64022.js"
+  },
+  {
+    "revision": "5b84f03b7e72a1366826178fef9af977",
+    "url": "https://testtmtelos.github.io/csb-usj5x/static/media/burger.5b84f03b.png"
+  },
+  {
+    "revision": "65fd5b77ad79d908b38f1b913c392140",
+    "url": "https://testtmtelos.github.io/csb-usj5x/static/media/ground.65fd5b77.png"
+  },
+  {
+    "revision": "ec3df7229d04b694254033fd6285ac9a",
+    "url": "https://testtmtelos.github.io/csb-usj5x/static/media/houses.ec3df722.png"
+  },
+  {
+    "revision": "746323a4ffd8540c15beb78e0ff61eec",
+    "url": "https://testtmtelos.github.io/csb-usj5x/static/media/kitchen.746323a4.png"
+  },
+  {
+    "revision": "5a4af241bd236b06331ab0a2f8c1fbee",
+    "url": "https://testtmtelos.github.io/csb-usj5x/static/media/plate.5a4af241.png"
+  },
+  {
+    "revision": "20e01a93f886b54ddf3986b6d316317d",
+    "url": "https://testtmtelos.github.io/csb-usj5x/static/media/room.20e01a93.png"
+  },
+  {
+    "revision": "898621f780a0ccca7c8ebc3ff182b451",
+    "url": "https://testtmtelos.github.io/csb-usj5x/static/media/room.898621f7.png"
+  },
+  {
+    "revision": "e53fae9282e91168d26a52a165ff960e",
+    "url": "https://testtmtelos.github.io/csb-usj5x/static/media/room.e53fae92.png"
+  },
+  {
+    "revision": "436bbe5a58b96c85ca57494db02dff6e",
+    "url": "https://testtmtelos.github.io/csb-usj5x/static/media/signboard.436bbe5a.png"
+  },
+  {
+    "revision": "38382a5240de34e210138e2b29c99191",
+    "url": "https://testtmtelos.github.io/csb-usj5x/static/media/table.38382a52.png"
+  },
+  {
+    "revision": "aa4e1286ab429892c5fd0ec0b268dadb",
+    "url": "https://testtmtelos.github.io/csb-usj5x/static/media/winner.aa4e1286.png"
+  },
+  {
+    "revision": "6a9cdb6b1f93f38913529f0ee73d31f4",
+    "url": "https://testtmtelos.github.io/csb-usj5x/static/media/wooden-floor.6a9cdb6b.png"
+  }
+]);
